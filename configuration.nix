183aeaeb43b195302c8bb4c0f72d5d892157946c
Mahmoud Elghazaly <105427440+Mahmoudelghazaly@users.noreply.github.com>
2025-12-06 00:17:45 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -66,7 +66,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.elghazaly = {
     isNormalUser = true;
-    initialPassword = "123456";
+    initialPassword = "12456";
     extraGroups = [ "networkmanager"  "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
